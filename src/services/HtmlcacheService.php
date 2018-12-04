e833228e2d6b876ac467f862d2f093947141a04f
JamesToothill <james@rework.digital>
2018-12-04 15:08:22 +0000

    Update to ensure setting.json is used..
    Then fall back to the setting screen and then a default value..

--- src/services/HtmlcacheService.php
+++ src/services/HtmlcacheService.php
@@ -247,7 +247,9 @@ class HtmlcacheService extends Component
      */
     private function loadCache($file)
     {
-        if (!empty($this->settings->cacheDuration)) {
+        if (file_exists($settingsFile = $this->getDirectory() . 'settings.json')) {
+            $settings = json_decode(file_get_contents($settingsFile), true);
+        } elseif (!empty($this->settings->cacheDuration)) {
             $settings = ['cacheDuration' => $this->settings->cacheDuration];
         } else {
             $settings = ['cacheDuration' => 3600];
