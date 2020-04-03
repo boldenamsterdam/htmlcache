6ea97f983491cd6de59b1a3a802fec863a2e2973
Adrian Block <hello@thisisablock.com>
2020-04-03 08:10:27 +0200

    changed cache identifier from param to internal uri

--- src/HtmlCache.php
+++ src/HtmlCache.php
@@ -142,10 +142,14 @@ class HtmlCache extends Plugin
                 if ($this->htmlcacheService->canCreateCacheFile()) {
                     $elementClass = get_class($event->element);
                     if (!in_array($elementClass, [User::class, GlobalSet::class])) {
-                        $uri = \Craft::$app->request->getParam('p', '');
+                        $uri = \Craft::$app->request->getPathInfo() ?: $event->element->uri;
                         $siteId = \Craft::$app->getSites()->getCurrentSite()->id;
                         $elementId = $event->element->id;
-                        
+
+                        if (!$uri || strlen($uri) === 0) {
+                            return;
+                        }
+
                         // check if cache entry already exits otherwise create it
                         $cacheEntry = HtmlCacheCache::findOne(['uri' => $uri, 'siteId' => $siteId]);
                         if (!$cacheEntry) {
